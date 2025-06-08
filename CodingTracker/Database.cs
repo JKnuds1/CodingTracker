285de073d86357294c6c0ea8d4f58d0645d0f31f
Jørgen Knudsen <jknuds1@hotmail.com>
2025-06-08 22:58:27 +0200

    Created the classes Database, Menu and CodingSession. As well as created CreateDatabase method in Databaseclass.

---
+++ CodingTracker/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace CodingTracker
+{
+    class Database
+    {
+        
+        
+        internal void GetDateTime()
+        {
+           
+        }
+        internal void CreateDatabase()
+        {
+            string dbPath = "Test.db";
+            string connectionString = $"Data Source={dbPath};Version = 3;";
+
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+            }
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string createTableSql = @"CREATE TABLE IF NOT EXISTS CodingTracker (
+                                          Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                          Name TEXT NOT NULL);";
+                using (var command = new SQLiteCommand(createTableSql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+
+            }
+        }
+
+    }
+}
--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
+using System.Data.SQLite;
 namespace CodingTracker
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Database db = new Database();
+            db.CreateDatabase();
         }
     }
 }
